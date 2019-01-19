1ec379dbd7ed849788a33552cc700d95e84d8ce7
ksh <ksheremet@satelliz.com>
2019-01-19 16:23:58 +0200

    add

---
+++ exam2/js/script_scroll.js
@@ -0,0 +1,72 @@
+$(function(){
+       $('._scroll').on('click',function(){
+         $('html').animate({
+           scrollTop: $('._scrollTo').offset().top
+         }, 5000)
+       });
+     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // script.
+   //     $(function(){
+   //     $('.about-us_scroll').on('click',function(){
+   //     var n = $('._scrollToAbout').offset().top;
+   //     $('html').animate({
+   //     scrollTop: $('._scrollToAbout').offset().top}, 2000);
+   //     });
+   //     });
+   // script.
+   //     $(function(){
+   //     $('.project_scroll').on('click',function(){
+   //     var n = $('._scrollToProject').offset().top;
+   //     $('html').animate({
+   //     scrollTop: $('._scrollToProject').offset().top}, 2000);
+   //     });
+   //     });
+   // script.
+   //     $(function(){
+   //     $('.news_scroll').on('click',function(){
+   //     var n = $('._scrollToNews').offset().top;
+   //     $('html').animate({
+   //     scrollTop: $('._scrollToNews').offset().top}, 2000);
+   //     });
+   //     });
+   // script.
+   //     $(function(){
+   //     $('.contact_scroll').on('click',function(){
+   //     var n = $('._scrollToContact').offset().top;
+   //     $('html').animate({
+   //     scrollTop: $('._scrollToContact').offset().top}, 2000);
+   //     });
+   //     });
---
+++ exam2/js/script_slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.header_slider').slick({
+       slidesToShow: 1,
+       arrows: false,
+       variableWidth: true,
+       autoplay: true,
+       autoplaySpeed: 2000,
+       speed: 4000,
+       infinite: false,
+       dots: true,
+   });
+});
